e27a32c2aec7ba30f548ae5377ed73fed19aab27
vagrant <vagrant@homestead>
2023-06-23 15:09:28 +0000

    tailwind follow-up install

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
+import aspectRatio from '@tailwindcss/aspect-ratio';
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -7,6 +9,8 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
     ],
 
     theme: {
@@ -17,5 +21,9 @@ export default {
         },
     },
 
-    plugins: [forms],
+    plugins: [
+        typography,
+        forms,
+        aspectRatio
+    ],
 };
